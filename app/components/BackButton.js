fd8c9a7b50598a654064eca9056a9e565487c17b
nicolasgodineau <nicolasgodineau@live.fr>
2024-11-09 23:11:43 -0500

    ajustement du retour

--- app/components/BackButton.js
+++ app/components/BackButton.js
@@ -1,28 +1,17 @@
 "use client";
 
-import { useRouter } from 'next/navigation';
+import Link from 'next/link'; // Importer Link pour la navigation
 import Image from 'next/image';
 
-import bottle from '../icons/bottle.png'
+import bottle from '../icons/bottle.png';
+
 export default function BackButton() {
-    const router = useRouter(); // Initialiser le routeur
-
-    const handleBack = () => {
-        // Remonter en haut de la page avant de naviguer
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-        router.back(); // Retour à la page précédente
-    };
-
     return (
-        <div className='fixed bottom-0 left-0 right-0 h-8 w-full flex flex-row items-center justify-center bg-white '>
-
-            <button
-                onClick={handleBack}
-                className="flex flex-row items-center gap-8  text-black rounded transition"
-            >
-                <Image src={bottle} alt="Rouge" width={15} className="-rotate-90 " />
+        <div className='fixed bottom-0 left-0 right-0 h-8 w-full flex flex-row items-center justify-center bg-white p-2'>
+            <Link href="/" className="flex flex-row items-center gap-8 text-black rounded transition">
+                <Image src={bottle} alt="Retour" width={15} className="-rotate-90 " />
                 Retour
-            </button>
+            </Link>
         </div>
     );
 }
