707fb4b708fdf609d8ec1a47b56b46275a54d444
nicolasgodineau <nicolasgodineau@live.fr>
2024-11-26 14:14:43 -0500

    Création d'un composant pour les listes des pays et cépage

---
+++ app/components/ListAside.js
@@ -0,0 +1,44 @@
+"use client";
+import React from 'react';
+import Link from 'next/link';
+import RippleButton from './RippleButton';
+import GrappeRedSvg from './icons/GrappeRedSvg.js';
+import GrappeWhiteSvg from './icons/GrappeWhiteSvg.js';
+
+export default function ListAside({ title, data, isCountryData }) {
+    return (
+        <div className="my-4">
+            {/* Affichage du titre (h2) passé en props */}
+            <h2 className="text-h3 font-semibold text-secondaryLight text-left pl-2">{title}</h2>
+
+            {/* Vérification de la structure des données pour déterminer si c'est des pays ou des cépages */}
+            {data && data.length > 0 && (
+                <ul className="w-3/4 flex flex-col text-paragraph">
+                    {data.map((item) => (
+                        <li key={item.id} className='h-12 flex items-center'>
+                            <RippleButton className="flex items-center gap-2 px-3">
+                                <Link className="flex items-center gap-2" href={isCountryData ? `/pays/${item.id}` : `/cepages/${item.id}`}>
+                                    {isCountryData ? (
+                                        <>
+                                            <span className="text-h2">{item.flag}</span>
+                                            {item.name}
+                                        </>
+                                    ) : (
+                                        <>
+                                            {item.type.toLowerCase() === 'rouge' ? (
+                                                <GrappeRedSvg className="inline-block mr-2" width={30} height={30} />
+                                            ) : (
+                                                <GrappeWhiteSvg className="inline-block mr-2" width={30} height={30} />
+                                            )}
+                                            {item.name}
+                                        </>
+                                    )}
+                                </Link>
+                            </RippleButton>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+}
