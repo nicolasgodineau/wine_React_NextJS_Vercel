762358a00082ebd6a27a4b4a221cc716142fd342
nicolasgodineau <nicolasgodineau@live.fr>
2024-11-27 16:29:25 -0500

    bug fix

--- app/components/BackButton.js
+++ app/components/BackButton.js
@@ -1,15 +1,37 @@
-import Link from 'next/link';
-import ArrowBackSvg from '@components/icons/ArrowBackSvg.js';
-import RippleButton from './RippleButton.js';
+"use client";
 
-export default function BackButton() {
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import RippleButton from "./RippleButton";
+import ArrowBackSvg from "@components/icons/ArrowBackSvg";
+
+export default function BackButton({ className = "", effectWidth = "50px" }) {
+    const router = useRouter();
+    const [canGoBack, setCanGoBack] = useState(false);
+
+    useEffect(() => {
+        // Vérifie si l'utilisateur peut revenir en arrière
+        setCanGoBack(window.history.length > 1);
+    }, []);
+
+    const handleBack = () => {
+        if (canGoBack) {
+            router.back();
+        }
+    };
+
     return (
-        <div className='fixed top-0 left-0 m-2 flex flex-row items-center justify-center text-secondary text-small backdrop-blur-xl bg-neutral-300/50 shadow-lg rounded-full cursor-pointer transition-transform hover:scale-110 z-10'>
-            <RippleButton className="px-2 py-2 rounded-full">
-                <div href="/" className="flex items-center">
-                    {/* <ArrowBackSvg width="20px" height="40px" fill="#4c0519" /> */}
-                    Retour
-                </div>
+        <div className="fixed top-0 left-0 m-2">
+            <RippleButton
+                onClick={handleBack}
+                className={`flex items-center gap-2 px-4 py-2 bg-neutral-300/50 backdrop-blur-xl shadow-lg rounded-full ${canGoBack ? "" : "opacity-50 cursor-not-allowed"
+                    } ${className}`}
+                effectWidth={50}
+                effectHeight={50}
+                disabled={!canGoBack}
+            >
+                {/*             <ArrowBackSvg width="24px" height="24px" fill="#4c0519" />
+ */}            Retour
             </RippleButton>
         </div>
     );
