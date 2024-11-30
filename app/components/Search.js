4c079ee6a7f647f78ee711f6f2919df49294281e
nicolasgodineau <nicolasgodineau@live.fr>
2024-11-30 13:11:50 -0500

    ajustement css

--- app/components/Search.js
+++ app/components/Search.js
@@ -48,32 +48,32 @@ export default function Search({ setIsSearchOpen }) {
     };
 
     return (
-        <div className="w-full flex flex-col-reverse items-stretch gap-4 pb-2 pt-4 text-paragraph ">
-            <div className="flex gap-1 rounded-xl bg-neutral-100/50 p-1">
+        <div className="w-full flex flex-col-reverse items-stretch gap-4 pb-2 text-paragraph ">
+            <div className="flex gap-1 rounded-lg bg-neutral-100/50 p-1">
                 <input
                     type="text"
                     value={query}
                     onChange={(e) => setQuery(e.target.value)}
                     placeholder="Rechercher"
-                    className="bg-transparent w-5/6 rounded-xl p-2"
+                    className="bg-transparent w-5/6 rounded-lg p-2"
                 />
-                <RippleButton className="w-1/2 rounded-xl bg-neutral-300/40 px-4 py-2 disabled:opacity-50" effectWidth={100} effectHeight={100} onClick={handleSearch}
+                <RippleButton className="w-1/2 rounded-lg bg-neutral-300/40 px-4 py-2 disabled:opacity-50" effectWidth={100} effectHeight={100} onClick={handleSearch}
                     disabled={loading}>
                     {loading ? 'Recherche...' : 'Valider'}
                 </RippleButton>
             </div>
             {error && <p className="text-red mt-2">{error}</p>}
-            <ul className="w-full flex flex-col gap-4 ">
+            <ul className="w-full flex flex-col gap-4 pt-4">
                 {results.length > 0 ? (
                     results.map((result) => (
                         <li
                             onClick={() => setIsSearchOpen(false)}
                             key={result.id}
-                            className="flex justify-center items-center bg-neutral-100/80 rounded-xl shadow hover:bg-gray-100 text-paragraph"
+                            className="flex justify-center items-center bg-neutral-100/80 rounded-lg shadow pl-2 hover:bg-gray-100 text-paragraph"
                         >
                             {/* Condition pour afficher un émoji de drapeau ou l'image du cépage */}
                             {result.type === 'country' ? (
-                                <RippleButton className="w-full rounded-xl" effectWidth={150} effectHeight={150}>
+                                <RippleButton className="w-full rounded-lg" effectWidth={150} effectHeight={150}>
                                     <Link href={`/pays/${result.id}`} className="flex w-full items-center justify-start px-1 py-2 ">
                                         <>
                                             <span className="text-2xl mr-2">{result.icon}</span>
@@ -83,7 +83,7 @@ export default function Search({ setIsSearchOpen }) {
                                     </Link>
                                 </RippleButton>
                             ) : (
-                                <RippleButton className="w-full rounded-xl " effectWidth={150} effectHeight={150}>
+                                <RippleButton className="w-full rounded-lg " effectWidth={150} effectHeight={150}>
                                     <Link href={`/cepages/${result.id}`} className="flex w-full items-center justify-start px-1 py-2 ">
                                         <>
                                             {/* Vérifie si c'est un cépage de type "rouge" ou "blanc" */}
